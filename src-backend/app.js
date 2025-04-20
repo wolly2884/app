3a3c54f83df84252ec328f2bbc5df7d3ff559c7f
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-04-20 20:25:50 -0300

    Update app.js

--- src-backend/app.js
+++ src-backend/app.js
@@ -1,5 +1,5 @@
 // src-backend/app.js
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const helmet = require('helmet');
