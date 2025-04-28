af453fee64e9c5f22f35e912c1b4f0e9c61c3efc
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-04-28 22:14:14 +0000

    xxxx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 
 function App() {
-  const routes = useRoutes([
-    { path: '/login', element: <Login /> },
-    { path: '/sidebar', element: <Sidebar /> },
-    // Other routes
-  ]);
-
-  return routes;
+    return (
+        <div className="App">
+            <Login />        
+        </div>
+    );
 }
 
 export default App;
