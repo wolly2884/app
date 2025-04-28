9489534b0c8129346751a5ae1edf7b79cc402f24
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-04-28 22:52:23 +0000

    ghgh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
-import Login from './components/Login';
+import Login from '../public/Login';
 import Sidebar from './components/Sidebar';
 
 function App() {
