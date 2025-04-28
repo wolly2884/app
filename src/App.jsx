31ece31bb49cbea1849d27b6fde07fad978922bb
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-04-28 23:10:24 +0000

    kkjkj

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-import React from 'react';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
 
 function App() {
     return (
         <div className="App">
-            <Login />        
+            <Sidebar />        
         </div>
     );
 }
