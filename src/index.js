59341e80f97e8e628e49d338f9564c7621933bbf
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-04-29 01:56:32 +0000

    teste

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './login/Login';
-import SidebarApp from './components/SidebarApp';
+import SidebarApp from './SidebarApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
