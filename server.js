0ea3f47af023db2eb6fa0e4dc351d6f32a179c57
wolly2884 <117057806+wolly2884@users.noreply.github.com>
2025-05-23 12:34:59 +0000

    console.log(`Server running on port ${PORT}`);

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
+
+console.log(`Server running on port ${PORT}`);
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
